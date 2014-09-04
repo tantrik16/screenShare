e5ecf51540a27765444a793ff09d91b61b10b8ab
tantrik16 <rishi.jha15@gmail.com>
2014-09-04 23:17:30 +0530

    init

--- client.js
+++ client.js
@@ -1,4 +1,20 @@
-var socket = require('socket.io-client')('http://mobile-cmd.herokuapp.com');
-socket.on('command', function (data){
+var socket = require('socket.io-client')('http://localhost:5001');
+var my_credentials = []
+socket.on('command', function (data){	
 	console.log(data);
 });
+socket.on('user', function (data){
+	if(data == 'No user'){
+		console.log("No such user Found :( !");
+		process.exit(0);
+	}
+	console.log(data);
+});
+if(!process.argv[2] || !process.argv[3]){
+	console.log('Enter Username & Password!');
+	process.exit(0);
+}
+my_credentials = my_credentials.concat(process.argv[2]);
+my_credentials = my_credentials.concat(process.argv[3]);
+console.log(my_credentials);
+socket.emit('credentials', my_credentials);
--- server.js
+++ server.js
@@ -5,16 +5,117 @@ var express = require('express'),
 	querystring = require('querystring'),
 	server = require('http').Server(app),
 	io = require('socket.io').listen(server);
-var sockets = {}; 
+
+
+var sockets = {};
+var system = {};
+var loggedIn = {};
+
+mongoose.connect('mongodb://tantrik:tantrik1115@ds037907.mongolab.com:37907/heroku_app26645381');
+var userDB = new mongoose.Schema({
+	username : 'string',
+	password : 'string'
+});
+
+var User = mongoose.model('userDB', userDB);
+User.find(function (err,res){
+	if(err)
+		return;
+	console.log(res);
+});
 io.on('connection', function (socket){
 	console.log('connected');
 	sockets[socket.id] = socket;
+	socket.on('credentials', function (data){
+		console.log(data[0] + " " + data[1]);
+		User.findOne({username : data[0]}, function (err, res){
+			if(err){
+				console.log(err);
+				return;
+			}
+			if(res == null){
+				socket.emit('user', 'No user');
+				return;
+			}
+			if(res['password'] == data[1]){
+				socket.emit('user', 'Authentication Successful :)')
+				system[data[0]] = socket;
+			}
+			else{
+				socket.emit('user', 'No user');
+			}
+		});
+	});
+	socket.on('disconnect', function (){
+		delete sockets[socket.id];
+	});
 });
+
+app.use(express.cookieParser('1234567890QWERTYoolalathisisastupidkey'));
+app.use(express.session({secret: '1234567890QWERTYoolalathisisastupidkey'}));
+app.engine('.html', require('ejs').__express);
+
+app.get('/', function (req, res){
+	if(!loggedIn[req.sessionID]){
+		res.render('index.ejs');
+	}
+	else{
+		res.render('loggedin.ejs', {username : loggedIn[req.sessionID]});
+	}
+});
+
+app.get('/open/:process', function (req, res){
+	if(!loggedIn[req.sessionID]){
+		res.end();
+		return;
+	}
+	if(system[loggedIn[req.sessionID]]){
+		system[loggedIn[req.sessionID]].emit('command', req.params.process);
+		res.end('Started!');
+	}
+	else{
+		res.end('Client not working on your system!');
+	}
+});
+app.post('/login', function (req, res){
+	var chunk = '';
+	req.on('data', function (data){
+		chunk += data;
+	});
+	req.on('end', function (){
+		var string = querystring.parse(chunk);
+		console.log(string);
+		User.findOne({username : string['username']}, function (err, rows){
+			if(err){
+				console.log(err);
+				return;
+			}
+			if(rows == null){
+				res.redirect('/');
+				return;
+			}
+			if(rows['password'] == string['password']){
+				loggedIn[req.sessionID] = string['username'];
+				res.redirect('/');
+				return;
+			}
+			else{
+				res.redirect('/');
+			}
+		});
+	});
+});
+
+/*
 app.use(function (req, res, next){
 	for(socket in sockets){
 		sockets[socket].emit('command', 'vlc');
 	}
 	next();
 });
+*/
+
 app.use(express.static(__dirname + '/public'));
+
+
 server.listen(Number(process.env.PORT || 5001));
